0978b9a410670d97b0a8f9284429397a9430fcb1
1hiteshk <1hiteshk@gmail.com>
2024-01-31 19:34:12 +0530

    Added footer component and some styling is done

--- src/components/Layout/Section.tsx
+++ src/components/Layout/Section.tsx
@@ -11,8 +11,8 @@ const Section = (props: any) => {
       gap={props.gap || "20px"}
       position={"relative"}
       backgroundColor={props?.backgroundColor || "#17131e" }
-      borderRadius={"15px"}
-      padding="30px 40px"
+      borderRadius={props?.borderRadius || "15px"}
+      padding={props?.padding || "30px 40px"}
       width={props?.width || "full"}
   
     >
