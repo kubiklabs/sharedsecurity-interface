6585a57953b3948cc9a19dfe0e9b96818dfc3191
Deepak <96366840+DeepakS-Github@users.noreply.github.com>
2024-02-26 20:53:00 +0530

    Doughnut chart (#28)
    * Others label removal in doughnut chart if it is 0
    
    * Rename 'Amount' column to 'Total Supply' in assets page
    
    * Pie chart and table data on all netwok  only appear when all the three network assets have been loaded
    
    * Fix: Update Table Amount When Switching from All Networks to Neutron
    
    * Value change to Total Amount and $ sign fix
    
    * Recharts dougnut chart replace to echarts doughnut chart
    
    * Add function to format large numbers with suffixes in doughnut chart

--- src/components/Graphs and Chart/DoughnutGraph.tsx
+++ src/components/Graphs and Chart/DoughnutGraph.tsx
@@ -1,64 +1,61 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import ReactECharts from "echarts-for-react";
 
 const DoughnutGraph = ({ doughnutData }: { doughnutData: any[] }) => {
-  const option = {
-    // legend: {
-    //   orient: "vertical",
-    //   left: "left",
-    //   data: doughnutData.map((item) => item.name)
-    // },
-    series: [
-      {
-        name: "Dummy Data",
-        type: "pie",
-        radius: ["50%", "80%"],
-        data: doughnutData,
-        label: {
-          show: true,
-          formatter: function (params: any) {
-            return `{a|$${params.data.value.toLocaleString("en", {
-              maximumFractionDigits: 3,
-            })}}\n{b|${params.name}:} {c|${params.percent}%}`;
-          },
-          rich: {
-            a: {
-              color: "white",
-              fontSize: "16px",
-              height: 25,
-            },
-            b: {
-              color: "white",
-              fontSize: "14px",
-            },
-            c: {
-              color: "white",
-              fontSize: "12px",
-            },
-          },
-        },
 
-        itemStyle: {
-          emphasis: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-        },
-      },
-    ],
-  };
+    const option = {
+        // legend: {
+        //   orient: "vertical",
+        //   left: "left",
+        //   data: doughnutData.map((item) => item.name)
+        // },
+        series: [
+            {
+                name: "Dummy Data",
+                type: "pie",
+                radius: ["50%", "80%"],
+                data: doughnutData,
+                label: {
+                    show: true,
+                    formatter: function (params: any) {
+                        console.log(params);
+                        return `{a|$${params.data.shortValue}}\n{b|${params.name}:} {c|${params.percent}%}`;
+                    },
+                    rich: {
+                        a: {
+                            color: "white",
+                            fontSize: "16px",
+                            height: 25,
+                        },
+                        b: {
+                            color: "white",
+                            fontSize: "14px",
+                        },
+                        c: {
+                            color: "white",
+                            fontSize: "12px",
+                        },
+                    },
+                },
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: "rgba(0, 0, 0, 0.5)",
+                    },
+                }
+            },
+        ],
+    };
 
-  console.log(option);
-
-  return (
-    <Box height={"100%"} width={"100%"}>
-      <ReactECharts
-        option={option}
-        style={{ width: "100%", height: "100%", overflow: "visible" }}
-      />
-    </Box>
-  );
+    return (
+        <Flex height={"100%"} width={"100%"} justifyContent={"center"} alignItems={"center"}>
+            <ReactECharts
+                option={option}
+                style={{ width: "100%", overflow: "visible" }}
+            />
+        </Flex>
+    );
 };
 
 export default DoughnutGraph;
