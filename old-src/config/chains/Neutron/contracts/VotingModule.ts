0a352c51312af83d1d89c1b2bc45fa5d9f6d336f
DeepakS-Github <deepakanonymous75@gmail.com>
2024-03-05 13:21:16 +0530

    old-src remove

--- old-src/config/chains/Neutron/contracts/VotingModule.ts
+++
@@ -1,4 +0,0 @@
-export const neutronVotingModule = {
-  name: "neutronVotingModule",
-  at: "neutron1f6jlx7d9y408tlzue7r2qcf79plp549n30yzqjajjud8vm7m4vdspg933s",
-};
--- old-src/utils/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IVoteValueProps {
-  YES: string;
-  NO: string;
-  ABSTAIN: string;
-  VETO: string;
-}
