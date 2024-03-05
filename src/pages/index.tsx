9f9f6a8fe0056b19a642051ae789b8067caa8149
DeepakS-Github <deepakanonymous75@gmail.com>
2024-03-05 11:44:34 +0530

    Default overview page open when server run fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const index = () => {
-  return (
-    <div>index</div>
-  )
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/overview');
+  }, []);
+
+  return null;
 }
 
 export default index
