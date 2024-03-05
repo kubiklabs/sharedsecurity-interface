e376ad37e9ca8bb6fc8fec3e3434c78d460b390b
DeepakS-Github <deepakanonymous75@gmail.com>
2024-03-05 15:41:49 +0530

    Added the meta description in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
         <link rel="icon" type="image/png" href="/kubik-logo-white.png" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </Head>
       <body>
         <Main />
